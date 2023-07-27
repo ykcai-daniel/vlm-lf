02179e1213b30ff8665a2691ed6eb59b9355672d
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2023-07-27 17:49:42 +0800

    test case

--- Readme.md
+++ Readme.md
@@ -4,4 +4,8 @@ To prepare the Hong Kong airport video, first download this [video](https://www.
 ```
 ffmpeg -i hong_kong_airport.mp4 -c copy -map 0 -segment_time 00:03:00 -f segment -reset_timestamps 1 hong_kong_airport%01d.mp4
 ```
+You can also convert a video into frame with certain fps with the following command
+```
+ffmpeg -i hong_kong_airport_3.mp4 -vf fps=5 img%04d.jpg
+```
 
--- test_cases.py
+++ test_cases.py
@@ -1,5 +1,26 @@
 #for video chunk 3 
+#from 8:00 to 9:40 (100s)
+#Resample the video at 6fps (5 frames per second), frame index starts from zero. The intervals are an array of tuples
 test_cases=[
-    ['white backpack','white suitcase','blue backpack','blue suitcase'],
-    ['white backpack','blue backpack','tote bag'],
+    {
+        'query':'white backpack',
+        'intervals':[],
+    },
+    {
+        'object':'white suitcase'
+    },
+    {
+        #negative sample: should not exist in any frame
+        'object':'steak'
+    },
+    {
+        'object':'black_backpack'
+    },
+
+    {
+        'object':'tote bag'
+    }
+    # two more novel luggages (described with color/shape)
+    # two mans and two women (one with plain clothing and the other with fancier clothing)
+    #images of man and woman and the novel luggages placed in test_images
 ]
