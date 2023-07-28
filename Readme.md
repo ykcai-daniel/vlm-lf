e43e6190fbe752bc06e78005a56e1c8bb58a9a8e
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2023-07-28 10:03:24 +0800

    test cases

--- Readme.md
+++ Readme.md
@@ -4,7 +4,10 @@ To prepare the Hong Kong airport video, first download this [video](https://www.
 ```
 ffmpeg -i hong_kong_airport.mp4 -c copy -map 0 -segment_time 00:03:00 -f segment -reset_timestamps 1 hong_kong_airport%01d.mp4
 ```
-You can also convert a video into frame with certain fps with the following command
+We have chosen the part between 8:00 to 9:40 as our test data, you can either generate it with ffmpeg, or download
+from [google drive link](https://drive.google.com/file/d/1NW670p5VUBKKNl2oZfA80VCNNgit7kcY/view?usp=drive_link)
+
+To convert a video into frame with certain fps with the following command
 ```
 ffmpeg -i hong_kong_airport_3.mp4 -vf fps=5 img%04d.jpg
 ```
--- test_cases.py
+++ test_cases.py
@@ -1,24 +1,32 @@
 #for video chunk 3 
 #from 8:00 to 9:40 (100s)
-#Resample the video at 6fps (5 frames per second), frame index starts from zero. The intervals are an array of tuples
+#Resample the video at 5fps, frame index starts from zero. The intervals are an array of tuple of (start,end)
 test_cases=[
     {
         'query':'white backpack',
         'intervals':[],
     },
     {
-        'object':'white suitcase'
+        'object':'white suitcase',
+        'intervals':[],
     },
     {
         #negative sample: should not exist in any frame
-        'object':'steak'
+        'object':'steak',
+        'intervals':[],
     },
     {
-        'object':'black_backpack'
+        'object':'black backpack',
+        'intervals':[],
     },
 
     {
-        'object':'tote bag'
+        'object':'tote bag',
+        'intervals':[],
+    },
+    {
+        'object':'white_shirt_blue_jeans_man.jpg',
+        'intervals':['00:17','00:20'],
     }
     # two more novel luggages (described with color/shape)
     # two mans and two women (one with plain clothing and the other with fancier clothing)
