0b08ea47cdce3661a9f2c9366c9fa04a5474fb58
Daniel-YH-Cai <1155141377@link.cuhk.edu.hk>
2023-07-31 15:06:44 +0800

    clip baseline

--- plot.py
+++ plot.py
@@ -3,11 +3,16 @@ import json
 import numpy as np
 
 if __name__=='__main__':
-    with open('hong_kong_airport_3.json','r') as f:
+    with open('data/hong_kong_airport_demo_data.mp4_scores.json','r') as f:
         results=json.load(f)
     print(results)
-    results_np={ name:np.array(logits) for name,logits in results.items()}
+    results_np={ name:np.array(logits) for name,logits in results['result'].items()}
+    
+    fig, ax = plt.subplots(figsize=(12, 6))
     for name,result in results_np.items():
-        plt.plot(result,label=name)
+        ax.plot(result,label=name)
     plt.legend(loc="upper left")
+    plt.ylim([0, 35])
+    plt.ylabel('Cosine similarity between each language query and  the image')
+    plt.xlabel('Frame sampled at 10fps')
     plt.savefig('results.jpg')
