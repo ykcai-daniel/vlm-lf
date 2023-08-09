8303b574c853f655b83a63e1907e4a190faf0303
Srijan Srivastava <srijan.srivastav2@gmail.com>
2023-08-09 16:44:38 +0800

    cleaning code of lf.py

--- lf.py
+++ lf.py
@@ -9,77 +9,41 @@ from test_cases import test_cases
 from frame_processor import vlm_processor,visualize_results_lang,visualize_result_image
 import torch
 
-#TODO: remove duplicate code
-def process_video_owl_lang(video_path:str,text_queries,interval=6,result_dir=None,max_frame=None,result_video=None,run_type='lang'):
-    print(f"Searching with Language Queries: {text_queries} in video {video_path}")
-    device='cpu'
+def process_video_owl(video_path:str,text_queries=None,image_queries=None,interval=6,result_dir=None,max_frame=None,result_video=None,run_type='both'):
+    if text_queries is None and image_queries is None:
+        print('Suply either image query or lang query')
+        return
+    if text_queries is not None and image_queries is not None:
+        print('Suply either image query or lang query')
+        return
+    if text_queries is not None:
+        query_type='lang'
+    else:
+        query_type='image'
+    print(f"Searching with {query_type} Queries: {text_queries if text_queries else image_queries} in video {video_path}")
+    device='cuda' if torch.cuda.is_available() else 'cpu'
     all_frame_results=[]
-    if torch.cuda.is_available():
-        device='cuda'
     vlm_processor.model.to(device)
-    print("Using "+device)
-    video=cv2.VideoCapture(video_path)
-    if result_video is not None:
-        #codec must be avc1 (h.264) to allowing playing in <video> element of html
-        #however avc1 is not supported on linux if you install opencv-python with pip
-        #in backend, consider converting video with ffmpeg
-        #https://stackoverflow.com/questions/59290569/opencv-video-writer-unable-to-find-codec-or-avc1
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video_writer=cv2.VideoWriter(result_video,fourcc,round(30.0/interval),(int(video.get(3)),int(video.get(4))))
-    if result_dir is not None:
-        os.makedirs(f'./results/{result_dir}',exist_ok=True)
-    frame_count=0
-    print(f"Number of frames: {int(video.get(cv2.CAP_PROP_FRAME_COUNT))} Processing interval: {interval}")
-    while video.isOpened():
-        ret, frame = video.read()
-        if not ret:
-            break
-        #Very important: OpenCV read an image as BGR yet pytorch models assume an image is RGB
-        frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        if max_frame is not None:
-            if max_frame<=frame_count:
-                break
-        if frame_count%interval==0:
-            start=time.perf_counter()
-            result=vlm_processor.process_image(frame,text_queries,device)
-            result['frame']=frame_count
-            all_frame_results.append(result)
-            end=time.perf_counter()
-            print(f"Results of frame {frame_count}: {result['scores']} Time {end-start}s")
-            visualized_image=visualize_results_lang(frame,result,text_queries)
-            if result_dir is not None:
-                result_path=f'./results/{result_dir}/frame_{frame_count}.jpg'
-                cv2.imwrite(result_path,visualized_image)
-            if result_video is not None:
-                video_writer.write(visualized_image)
-        else:
-            all_frame_results.append({'frame':frame_count})
-        frame_count=frame_count+1
-    if result_video is not None:
-        video_writer.release()
-    video.release()
-    return all_frame_results
+    print("Using " + device)
 
-def process_video_owl_image(video_path:str,image_queries_names,interval=6,result_dir=None,max_frame=None,result_video=None,run_type='image'):
-    print(f"Searching with image queries {image_queries_names} in video {video_path}")
-    device='cpu'
-    all_frame_results=[]
-    if torch.cuda.is_available():
-        device='cuda'
-    vlm_processor.model.to(device)
-    print ('Using '+ device)
-    #convert BGR to RGB
-    image_queries=[cv2.imread(name) for name in image_queries_names]
-    image_queries=[cv2.cvtColor(i,cv2.COLOR_BGR2RGB) for i in image_queries]
-    video=cv2.VideoCapture(video_path)
-    if result_dir is not None:
-        os.makedirs(f'./results/{result_dir}',exist_ok=True)
+    if query_type=='image':
+        # convert BGR to RGB
+        image_queries=[cv2.imread(name) for name in image_queries]
+        image_queries=[cv2.cvtColor(i,cv2.COLOR_BGR2RGB) for i in image_queries]
+    
+    video = cv2.VideoCapture(video_path)
+
     if result_video is not None:
         #codec must be avc1 (h.264) to allowing playing in <video> element of html
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         video_writer=cv2.VideoWriter(result_video,fourcc,round(30.0/interval),(int(video.get(3)),int(video.get(4))))
+    
+    if result_dir is not None:
+        os.makedirs(f'./results/{result_dir}',exist_ok=True)
+    
     frame_count=0
     print(f"Number of frames: {int(video.get(cv2.CAP_PROP_FRAME_COUNT))} Processing interval: {interval}")
+
     while video.isOpened():
         ret, frame = video.read()
         if not ret:
@@ -89,14 +53,21 @@ def process_video_owl_image(video_path:str,image_queries_names,interval=6,result
         if max_frame is not None:
             if max_frame<=frame_count:
                 break
+        
         if frame_count%interval==0:
             start=time.perf_counter()
-            result=vlm_processor.image_query(frame,image_queries,device)
+            if query_type=='lang':
+                result=vlm_processor.process_image(frame,text_queries,device)
+            else:
+                result=vlm_processor.image_query(frame,image_queries,device)
             result['frame']=frame_count
             all_frame_results.append(result)
             end=time.perf_counter()
             print(f"Results of frame {frame_count}: {result['scores']} Time:{end-start}s")
-            visualized_image=visualize_result_image(frame,result,f"Image: {image_queries_names[0]}")
+            if query_type=='lang':
+                visualized_image=visualize_results_lang(frame,result,text_queries)
+            else:
+                visualized_image=visualize_result_image(frame,result,f"Image: {image_queries[0]}")
             if result_dir is not None:
                 result_path=f'./results/{result_dir}/frame_{frame_count}.jpg'
                 cv2.imwrite(result_path,visualized_image)
@@ -125,28 +96,20 @@ def run_video_to_video(video_name:str,queries:list[str],run_type:str,interval=3,
         result_video_name=None
     result_json_name=f'./results/{video_raw_name}{str_max_frame}_{formatted_time}_{run_type}.json'
     print(f'Result Video Path: {result_video_name}')
-    if run_type=='image':
-        result=process_video_owl_image(video_name,queries,interval,result_dir=None,max_frame=max_frame,result_video=result_video_name)
-        with open(result_json_name,'w') as f:
-            print(f"Result Json: {result_json_name}")
-            json.dump({
-                'query':queries,
-                'type':run_type,
-                'result':result,
-                
-            },f)
-    elif run_type=='lang':
-        result=process_video_owl_lang(video_name,queries,interval,result_dir=None,max_frame=max_frame,result_video=result_video_name)
-        with open(result_json_name,'w') as f:
-            print(f"Result Json: {result_json_name}")
-            json.dump({
-                'query':queries,
-                'type':run_type,
-                'result':result,
-                
-            },f)
-    else:
+    if run_type not in ['image','lang']:
         print("Invalid run_type: must be image or lang ")
+        return
+    if run_type=='image':
+        result=process_video_owl(video_name,image_queries=queries,interval=interval,result_dir=None,max_frame=max_frame,result_video=result_video_name)
+    elif run_type=='lang':
+        result=process_video_owl(video_name,text_queries=queries,interval=interval,result_dir=None,max_frame=max_frame,result_video=result_video_name)
+    with open(result_json_name,'w') as f:
+        print(f"Result Json: {result_json_name}")
+        json.dump({
+            'query':queries,
+            'type':run_type,
+            'result':result,
+        },f)
 
     
 #If you are using CUHK CSE slurm cluster
