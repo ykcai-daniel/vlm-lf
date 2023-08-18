9b777f59c4a832703ddaf7298f254bb41ee3e7cf
ykcai-daniel <1155141377@link.cuhk.edu.hk>
2023-08-18 17:42:27 +0800

    preserve shift in box transform

--- pattern_matcher.py
+++ pattern_matcher.py
@@ -1,6 +1,10 @@
 import numpy as np
 import time
 from outputs import FrameResult
+import json
+import cv2
+#TODO: match by class
+#TODO: weight each IOU by frame score
 
 
 #inputs:
@@ -34,7 +38,7 @@ def _iou(frame_boxes,transformed_boxes):
     return ious
 
 class ParemGrid:
-    def __init__(self,frame_w=1280,frame_h=720,sampling_rate=25) -> None:
+    def __init__(self,frame_w=1280,frame_h=720,sampling_rate=50) -> None:
         parem_grid=np.mgrid[0:frame_w:sampling_rate,0:frame_h:sampling_rate,0.125:1.5:0.125]
         self.parem_grid=np.reshape(parem_grid,(3,-1),'F') #F stands for Fortran style row major ordering
     def xy(self):
@@ -50,45 +54,42 @@ class Pattern:
     def __init__(self,patter_list) -> None:
         self.data=patter_list
         self._np=np.array(patter_list)
-        print(self._np.shape)
+        self.len=self._np.shape[0]
 
     def xywh(self):
         return self._np[:,[0,1,2,3]]
+    def xyxy(self):
+        return FrameResultNp.xywh2xyxy(self.xywh())
 
     def classes(self):
         self._np[:,4]
 
-    def get_wh(self):
-        return self._np[:,(2,3)]
-    def get_xy(self):
-        return self._np[:,(0,1)]
-
-
     #to preserve the shape, we must transform with xyxy format
     def transform_with_parem(self,parem:ParemGrid):
-        wh=np.expand_dims(self.get_wh(),(2))
-        x_y=np.expand_dims(self.get_xy(),(2))
+        xyxy=self.xyxy()
+        lower_point=np.expand_dims(xyxy[:,(0,1)],(2))
+        higher_point=np.expand_dims(xyxy[:,(2,3)],(2))
         k=np.expand_dims(parem.k(),(0,1))
         x_0_y_0=np.expand_dims(parem.xy(),(0))
-        hw_scaled=k*wh
-        xy_shifted=x_y+x_0_y_0
-        new_boxes=np.concatenate([xy_shifted,hw_scaled],(1))
+        lower_scaled,higher_scaled=k*lower_point,k*higher_point
+        lower_shifted=lower_scaled+x_0_y_0
+        high_shifted=higher_scaled+x_0_y_0
+        new_boxes=np.concatenate([lower_shifted,high_shifted],(1))
         return new_boxes
+    
+    def __len__(self):
+        return self.len
 
     def visualize(self):
-        image=cv2.imread("result_550.jpg")
+        image=cv2.imread('misc/frame_550.jpg')
         pattern_box_xyxy=FrameResultNp.xywh2xyxy(self._np).tolist()
         #different color for different bounding box
         for index,point in enumerate(pattern_box_xyxy):
             p1_x,p1_y,p2_x,p2_y=int(point[0]),int(point[1]),int(point[2]),int(point[3])
             #print(f"P1： ({p1_x},{p1_y}) P2: ({p2_x},{p2_y})")
-            print(f"Plotting box: {point}")
             cv2.rectangle(image,pt1=(p1_x,p1_y),pt2=(p2_x,p2_y),color=(0,255,0),thickness=3)
             cv2.putText(image, f"{index}",(p1_x+5, p1_y+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
         cv2.imwrite("pattern_visualized.jpg",image)
-    
-
-
 
 class FrameResultNp:
     @staticmethod
@@ -124,7 +125,6 @@ class FrameResultNp:
     def boxes_xyxy(self):
         return self._np[:,2:6]
 
-
 def match_box(ious_of_parem):
     return np.argmax(ious_of_parem,axis=1)
 
@@ -132,30 +132,18 @@ def match_pattern_impl(frame_result_boxes,transformed_parem_boxes):
     #iou result: len(pattern)*len(frame)*len(parem)
     iou_frame_pattern=_iou(frame_result_boxes,transformed_parem_boxes)
     #use scipy linear assign to decide optimal box assignment for each parem group
-    start=time.perf_counter()
     max_index=match_box(iou_frame_pattern)
-    # def func(a):
-    #     return scipy.optimize.linear_sum_assignment(np.reshape(a,(3,10)))[0]
-    # match_results=np.apply_along_axis(func,0,iou_frame_pattern.reshape(-1,iou_frame_pattern.shape[-1]))
-    # for parem_index in range(iou_frame_pattern.shape[2]):
-    #     match_results.append(scipy.optimize.linear_sum_assignment(iou_frame_pattern[:,:,parem_index])[0])
-    # print(match_results[3])
-    # match_results=np.stack(match_results,axis=1)
-    end=time.perf_counter()
     index_other_dim_1=np.tile(np.arange(max_index.shape[0]),(max_index.shape[1],1)).T
     index_other_dim_2=np.tile(np.arange(max_index.shape[1]),(max_index.shape[0],1))
     max_box_seleced=iou_frame_pattern[index_other_dim_1,max_index,index_other_dim_2]
-    print(f"Max index: {max_index}")
     score_parem=np.average(max_box_seleced,axis=0)
     # print(f"Scores of parem: {score_parem} shape: {score_parem.shape}")
     max_loc=np.argmax(score_parem)
     print(f"Max loc:{max_loc} Max Score:{score_parem[max_loc]}")
-    print(f"Run time: {end-start}")
     return max_loc
 
-import json
-import cv2
 if __name__=="__main__":
+    
     #use frame 550 of 100s video as test
     parem_grid=ParemGrid()
     #Person with two luggage pattern xywh
@@ -164,33 +152,35 @@ if __name__=="__main__":
         (50,0,75,200), #person
         (75+50,100,50,100), #luggage two
     ])
-    pattern.visualize()
     frame_result=FrameResult()
-    with open('pattern_test.json') as f:
+    with open('misc/pattern_test.json') as f:
         data=json.load(f)
         frame_result.from_data_dict(data)
     frame_result=FrameResultNp(frame_result)
     #size: len(pattern)*(x,y,w,h)*len(parem) 
+    start=time.perf_counter()
     transformed_boxes=pattern.transform_with_parem(parem_grid)
-    transformed_boxes=np.transpose(transformed_boxes,(2,0,1)).reshape((-1,4))
-    transformed_boxes=FrameResultNp.xywh2xyxy(transformed_boxes)
-    #TODO: remove hardcoded reshape!
-    transformed_boxes=transformed_boxes.reshape((-1,3,4,))
-    transformed_boxes=np.transpose(transformed_boxes,(1,2,0))
     max_parem_loc=match_pattern_impl(frame_result.boxes_xyxy(),transformed_boxes)
     print(f"Max Parem Loc: {max_parem_loc}")
     print(f"Max Parems: {parem_grid.parem_grid[:,max_parem_loc]}")
     best_boxes=transformed_boxes[:,:,max_parem_loc].tolist()
     print(f"Best Transformed Boxes: {best_boxes}")
-    image=cv2.imread("result_550.jpg")
-    #different color for different bounding box
+    end=time.perf_counter()
+    image=cv2.imread("misc/frame_550.jpg")
+    #visualize the original boxes:
+    for index,record in enumerate(frame_result.data):
+        point=record[2]
+        p1_x,p1_y,p2_x,p2_y=int(point[0]),int(point[1]),int(point[2]),int(point[3])
+        #print(f"P1： ({p1_x},{p1_y}) P2: ({p2_x},{p2_y})")
+        cv2.rectangle(image,pt1=(p1_x,p1_y),pt2=(p2_x,p2_y),color=(0,0,255),thickness=3)
+    #visualize the matched boxes
     for index,point in enumerate(best_boxes):
         p1_x,p1_y,p2_x,p2_y=int(point[0]),int(point[1]),int(point[2]),int(point[3])
         #print(f"P1： ({p1_x},{p1_y}) P2: ({p2_x},{p2_y})")
-        print(f"Plotting box: {point}")
         cv2.rectangle(image,pt1=(p1_x,p1_y),pt2=(p2_x,p2_y),color=(0,255,0),thickness=3)
         cv2.putText(image, f"{index}",(p1_x+5, p1_y+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
     cv2.imwrite("result_550_visualized.jpg",image)
+    print(f"Run time: {end-start}s")
     #return score for parem group
     #return result and best parems
     
